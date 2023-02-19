0fe640fa8a9b7928ca7fdddcddf7c98862d2d539
ahhnchangjun <qkqk1009@gmail.com>
2023-02-19 22:36:00 +0900

    아스키코드1

---
+++ src/Baekjoon6/P11654.java
@@ -0,0 +1,10 @@
+package Baekjoon6;
+
+import java.io.IOException;
+
+public class P11654 {
+    public static void main(String[] args) throws IOException {
+    int a = System.in.read();
+        System.out.println(a);
+    }
+}
