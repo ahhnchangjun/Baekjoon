284ab9c1726e07cf1e2d96c6359a09331265c31f
ahhnchangjun <qkqk1009@gmail.com>
2023-02-19 18:25:23 +0900

    버퍼 활용 더하기

---
+++ src/Baekjoon2/P15552.java
@@ -0,0 +1,23 @@
+package Baekjoon2;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class P15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+
+
+        }
+        bw.close();
+    }
+}
