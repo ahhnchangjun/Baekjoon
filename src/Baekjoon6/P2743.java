6e31049817ad9fe0d911d97633ce63b1bfb1e685
ahhnchangjun <qkqk1009@gmail.com>
2023-02-21 18:02:43 +0900

    단어 길이 재기

---
+++ src/Baekjoon6/P2743.java
@@ -0,0 +1,12 @@
+package Baekjoon6;
+
+import java.util.Scanner;
+
+public class P2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int N = word.length();
+        System.out.println(N);
+    }
+}
